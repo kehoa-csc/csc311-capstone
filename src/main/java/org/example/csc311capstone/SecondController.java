fc7f575a932298837fb14edcb31164d291090ab7
Jordan <Jordanbiggs2154@gmail.com>
2024-11-20 10:49:31 -0500

    updated SecondController, (not tested but should work) Added highlight colors to secondPage.css, added menuback fx id to second_view.fxml

--- src/main/java/org/example/csc311capstone/SecondController.java
+++ src/main/java/org/example/csc311capstone/SecondController.java
@@ -1,9 +1,12 @@
 package org.example.csc311capstone;
 import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -21,7 +24,7 @@ public class SecondController {
     private Button editButton;
 
     @FXML
-    private Label menuLabel;
+    private Label menuLabel,menuBack;
 
     @FXML
     private Button reconbdButton;
@@ -29,55 +32,42 @@ public class SecondController {
     @FXML
     private AnchorPane slider;
 
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//
-//
-//        opacityPane.setVisible(false);
-//
-//        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),pane1);
-//        fadeTransition.setFromValue(1);
-//        fadeTransition.setToValue(0);
-//        fadeTransition.play();
-//
-//        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5),pane2);
-//        translateTransition.setByX(-600);
-//        translateTransition.play();
-//
-//        drawerImage.setOnMouseClicked(event -> {
-//
-//
-//            opacityPane.setVisible(true);
-//
-//            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
-//            fadeTransition1.setFromValue(0);
-//            fadeTransition1.setToValue(0.15);
-//            fadeTransition1.play();
-//
-//            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),Pane2);
-//            translateTransition1.setByX(+600);
-//            translateTransition1.play();
-//        });
-//
-//        pane1.setOnMouseClicked(event -> {
-//
-//
-//
-//            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
-//            fadeTransition1.setFromValue(0.15);
-//            fadeTransition1.setToValue(0);
-//            fadeTransition1.play();
-//
-//            fadeTransition1.setOnFinished(event1 -> {
-//                pane1.setVisible(false);
-//            });
-//
-//
-//            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
-//            translateTransition1.setByX(-600);
-//            translateTransition1.play();
-//        });
+  //  @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+            slider.setTranslateX(-176);
+            menuLabel.setOnMouseClicked( event ->{
+            TranslateTransition slide = new TranslateTransition();
+            slide.setDuration(Duration.seconds(0.4));
+            slide.setNode(slider);
+
+            slide.setToX(0);
+            slide.play();
+
+            slider.setTranslateX(-176);
+
+            slide.setOnFinished((ActionEvent e)-> {
+                    menuLabel.setVisible(true);
+                    menuBack.setVisible(false);
+                } );
+        });
+          menuBack.setOnMouseClicked(event ->{
+              TranslateTransition slide = new TranslateTransition();
+              slide.setDuration(Duration.seconds(0.4));
+              slide.setNode(slider);
+
+              slide.setToX(-176);
+              slide.play();
+
+              slider.setTranslateX(0);
+
+              slide.setOnFinished((ActionEvent e)->{
+                  menuLabel.setVisible(true);
+                  menuBack.setVisible(false);
+              });
+          });
     }
+}
 
 
 
