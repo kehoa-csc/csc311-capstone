e755271a99ef942ea861f73fb5e933fd75f3d57a
Jordan <Jordanbiggs2154@gmail.com>
2024-11-20 00:17:55 -0500

    Updated second_view.fxml to the new design & added more images. Updated SecondController doesn't work yet so it's commented out

--- src/main/java/org/example/csc311capstone/SecondController.java
+++ src/main/java/org/example/csc311capstone/SecondController.java
@@ -1,5 +1,85 @@
 package org.example.csc311capstone;
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class SecondController {
 
-}
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Label menuLabel;
+
+    @FXML
+    private Button reconbdButton;
+
+    @FXML
+    private AnchorPane slider;
+
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//
+//
+//        opacityPane.setVisible(false);
+//
+//        FadeTransition fadeTransition=new FadeTransition(Duration.seconds(0.5),pane1);
+//        fadeTransition.setFromValue(1);
+//        fadeTransition.setToValue(0);
+//        fadeTransition.play();
+//
+//        TranslateTransition translateTransition=new TranslateTransition(Duration.seconds(0.5),pane2);
+//        translateTransition.setByX(-600);
+//        translateTransition.play();
+//
+//        drawerImage.setOnMouseClicked(event -> {
+//
+//
+//            opacityPane.setVisible(true);
+//
+//            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
+//            fadeTransition1.setFromValue(0);
+//            fadeTransition1.setToValue(0.15);
+//            fadeTransition1.play();
+//
+//            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),Pane2);
+//            translateTransition1.setByX(+600);
+//            translateTransition1.play();
+//        });
+//
+//        pane1.setOnMouseClicked(event -> {
+//
+//
+//
+//            FadeTransition fadeTransition1=new FadeTransition(Duration.seconds(0.5),pane1);
+//            fadeTransition1.setFromValue(0.15);
+//            fadeTransition1.setToValue(0);
+//            fadeTransition1.play();
+//
+//            fadeTransition1.setOnFinished(event1 -> {
+//                pane1.setVisible(false);
+//            });
+//
+//
+//            TranslateTransition translateTransition1=new TranslateTransition(Duration.seconds(0.5),pane2);
+//            translateTransition1.setByX(-600);
+//            translateTransition1.play();
+//        });
+    }
+
+
+
+
+
