ca66dfd546bb59833605e43e308b936facd25916
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2024-11-24 16:22:54 -0500

    clarity

--- src/main/java/org/example/csc311capstone/SecondController.java
+++ src/main/java/org/example/csc311capstone/SecondController.java
@@ -11,6 +11,7 @@ import javafx.util.Duration;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+//Librarian Book View
 public class SecondController {
 
 
--- src/main/java/org/example/csc311capstone/ThirdController.java
+++ src/main/java/org/example/csc311capstone/ThirdController.java
@@ -1,4 +1,5 @@
 package org.example.csc311capstone;
 
+//Librarian Patron view
 public class ThirdController {
 }
