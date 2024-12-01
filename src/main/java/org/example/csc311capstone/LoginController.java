85883d30024ab5a70355bcbdf3d583e0fa2bbfe5
andrewkehoe <157737128+andrewkehoe@users.noreply.github.com>
2024-12-01 18:11:56 -0500

    login error messages

--- src/main/java/org/example/csc311capstone/LoginController.java
+++ src/main/java/org/example/csc311capstone/LoginController.java
@@ -19,9 +19,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-/*
-    Currently doesn't work
- */
 public class LoginController implements Initializable {
 
 
@@ -33,6 +30,9 @@ public class LoginController implements Initializable {
 
     @FXML
     private PasswordField password;
+
+    @FXML
+    private Label loginStatus;
 
     private final PatronsTable patronsTable = new PatronsTable();
     private final ObservableList<Patron> patrons = patronsTable.listAllPatrons();
@@ -51,11 +51,15 @@ public class LoginController implements Initializable {
     @FXML
     void login(ActionEvent actionEvent) {
             if(username.getText().isEmpty() || password.getText().isEmpty()){
+                loginStatus.setText("                 Please fill all the fields.");
                 return;
             }
             if(userType.getValue().equals("Staff")) {
                if(username.getText().equals("staff") && password.getText().equals("staff")) {
                    openWindow(actionEvent,"view/librarian_view.fxml");
+               }
+               else {
+                   loginStatus.setText("                   Incorrect staff details.");
                }
 
             }else if(userType.getValue().equals("Student")) {
@@ -66,6 +70,8 @@ public class LoginController implements Initializable {
                 if(user.isPresent()) {
                     patronID = user.get().getID();
                     openWindow(actionEvent,"view/patron_self_service.fxml");
+                } else {
+                    loginStatus.setText("      User not found or login incorrect.");
                 }
             }else {
                 username.setText("");
